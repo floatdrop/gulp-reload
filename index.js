ef9d2a6600dd0ab5798c568ad91cc7161ae8de70
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-01-19 00:35:08 +0600

    Added check for gulpfile.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ module.exports = function () {
 	var duplex = new Duplex({ objectMode: true, allowHalfOpen: true });
 
 	duplex._write = function _write(file, encoding, done) {
+		if (!(/gulpfile.js/).test(file.path)) { return; }
+
 		gutil.log('gulp-reload saw',
 			gutil.colors.magenta(path.basename(file.path)),
 			'and reloading now');
