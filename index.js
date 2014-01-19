f32eff4ed19a67d686642f5dafd593444b43621b
Vsevolod Strukchinsky <floatdrop@gmail.com>
2014-01-19 11:37:23 +0600

    Fixup through behavior

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 'use strict';
 
 var gutil = require('gulp-util'),
-	Duplex = require('stream').Duplex,
+	through2 = require('through2'),
 	path = require('path');
 
 module.exports = function () {
-	var duplex = new Duplex({ objectMode: true, allowHalfOpen: true });
+	return through2.obj(function (file, encoding, done) {
+		this.push(file);
 
-	duplex._write = function _write(file, encoding, done) {
 		if (!(/gulpfile.js/).test(file.path)) { return; }
 
 		gutil.log('gulp-reload saw',
 			gutil.colors.magenta(path.basename(file.path)),
 			'and reloading now');
 
-		duplex.unpipe();
+		this.unpipe();
 
-		require('shelljs').exec(process.argv.join(' '));
+		if (!process.env.RELOAD) {
+			while (true) {
+				require('shelljs').exec('RELOAD=true ' + process.argv.join(' '));
+			}
+		}
 
 		process.exit(0);
 
 		done();
-	};
-
-	duplex._read = function _read() { };
-
-	return duplex;
+	});
 };
